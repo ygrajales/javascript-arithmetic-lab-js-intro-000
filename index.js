01c26020efb4d6b5ec55adef06315255c526c9fe
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-11 22:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-  return a + b 
+  return a + b
 }
 
 function subtract(a, b) {
@@ -7,7 +7,7 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a * b 
+  return a * b
 }
 
 function divide(a, b) {
